bd67a1cf66b494ee601358fb641d5843c3a2d03f
Docharm <dylee@cloocus.com>
2025-05-27 14:29:20 +0900

    재도전

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Git new 0527 code!!"
+    return "Git doyeon new code!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
